457b6f3909447affed87b9ef7eacf2a73a23ea0b
Joel Young <young.wjh@gmail.com>
2026-03-25 17:55:38 -0400

    begin linking styles, lay out html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Greet the console
 
 console.log("Hello World!!! It's me, your ghost")
-console.log("Initiating process in the console - joeliecakes")
+console.log("Dishing out on local network 8080 -- joeliecakes")
